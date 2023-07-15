c347a7a7da937061f3d2f4f04677432390412db7
Hasan Huseynli <hasansh@code.edu.az>
2023-07-15 15:36:31 +0400

    blogAdded

--- assets/js/cart.js
+++ assets/js/cart.js
@@ -17,3 +17,82 @@ function decrementValue()
     }
 
 }
+
+
+
+
+
+
+
+
+
+function incrementValuebtn()
+{
+    var value = parseInt(document.getElementById('number').value, 10);
+    value = isNaN(value) ? 0 : value;
+    if(value<10){
+        value++;
+            document.getElementById('number').value = value;
+    }
+}
+function decrementValuebtn()
+{
+    var value = parseInt(document.getElementById('number').value, 10);
+    value = isNaN(value) ? 0 : value;
+    if(value>1){
+        value--;
+            document.getElementById('number').value = value;
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+const countryMenu = document.querySelector(".country-menu"),
+    countryBtn = countryMenu.querySelector(".country-btn"),
+    countries = countryMenu.querySelectorAll(".country"),
+    cBtn_text = countryMenu.querySelector(".cBtn-text");
+
+countryBtn.addEventListener("click", ()=>countryMenu.classList.toggle("active"));    
+
+countries.forEach(country => {
+    country.addEventListener("click", () => {
+        let selectedCountry = country.querySelector(".country-text").innerText;
+        cBtn_text.innerText = selectedCountry;
+        countryMenu.classList.remove("active")
+    })
+
+})      
+
+
+
+
+
+
+
+
+const stateMenu = document.querySelector(".state-menu"),
+    selectBtn = stateMenu.querySelector(".select-btn"),
+    options = stateMenu.querySelectorAll(".option"),
+    sBtn_text = stateMenu.querySelector(".sBtn-text");
+
+selectBtn.addEventListener("click", ()=>stateMenu.classList.toggle("active"));    
+
+options.forEach(option => {
+    option.addEventListener("click", () => {
+        let selectedOption = option.querySelector(".option-text").innerText;
+        sBtn_text.innerText = selectedOption;
+        stateMenu.classList.remove("active")
+    })
+
+})     
